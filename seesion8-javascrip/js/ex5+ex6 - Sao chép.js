4439aa31c9e68adc0839b864099d01a44b37e465
Phao Thu <phaothu591997@gmail.com>
2018-11-06 18:10:40 +0700

    delete copy

--- seesion8-javascrip/js/ex5+ex6 - Sao chép.js
+++
@@ -1,9 +0,0 @@
-var name = prompt(' xin mời nhập tên: ');
-var arrName = name.split(" ");
-document.write('họ ' + arrName[0] + ' chuyển thành chữ hoa: ' + arrName[0].toUpperCase());
-document.write('<br>');
-arrName.shift();
-arrName.pop();
-document.write(arrName.valueOf());
-arrName.toLowerCase();
-document.write('tên lót trong tên '+ name + ' chuyển thành chữ thường là: '+ arrName);
