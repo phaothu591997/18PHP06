1b12d1f0a9e2a99eba05006cf28d3851f62f28b9
Phao Thu <phaothu591997@gmail.com>
2018-11-22 17:59:43 +0700

    web cat toc

---
+++ seesion10/js/slider-product.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+// 	var sttt= 0;
+// 	starImg = $(".slider-product:first").attr("sttt");
+// 	endImg = $(".slider-product:last").attr("sttt");
+// 	$("div-slider").each(function(){
+// 		if($(this).is(':visible')){
+// 			sttt = $(this).attr("sttt");
+// 		}
+// 	});
+// 	$("#next1").click(function(){
+// 		if(sttt==endImg){
+// 			sttt=-1;
+// 		}
+// 		next=++sttt;
+// 		$(".slider-product").hide();
+// 		$(".slider-product").eq(next).show();
+
+// 	});
+// 	$("#prev1").click(function(){
+// 		if(sttt==0){
+// 			sttt=endImg;
+// 			prev= sttt++;
+// 		}	
+// 		prev=--sttt;
+// 		$(".slider-product").hide();
+// 		$(".slider-product").eq(prev).show();
+// 	});
+
+	// setInterval(function(){
+	// 	$("#next1").click();
+	// },3000);
+
+});
